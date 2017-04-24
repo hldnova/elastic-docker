21205c5ad5e0a41c9bde7e47706362408c196eb7
Lida He <lida.he@dell.com>
2017-04-24 09:28:34 -0400

    Move the script to bin directory

---
+++ bootstrap/bin/boot_lib.sh
@@ -0,0 +1,219 @@
+#!/bin/sh
+########################################################################
+# Boot script gets executed whenever the container is created
+# Use this script to install/configure any bits on the container
+# This script file should be available under /vipr-share folder
+# in the host machine and the folder will be visible to the container
+########################################################################
+
+set -ue
+export PATH=/bin:/bin:/sbin:/usr/bin:/usr/sbin
+
+SHARED_LOGS_VOLUME='/var/log'
+LOG_FILE=${SHARED_LOGS_VOLUME}/boot.log
+INSTALL_BASE='/opt/emc/nautilus/controller'
+DASHBOARD_BASE=${BOOT_LOCATION}/../
+VISUALIZATION_PATH=${DASHBOARD_BASE}/visualization
+SEARCH_PATH=${DASHBOARD_BASE}/search
+DASHBOARD_PATH=${DASHBOARD_BASE}/dashboard
+NAVIGATION_INDEX=navigation
+ES_ENDPOINT=${ES_ENDPOINT:-http://elasticsearch:9200}
+
+log(){
+    msg=" $*"
+    echo $(date "+%m%d%Y %T") : $msg
+    echo $(date "+%m%d%Y %T"):  $msg >> ${LOG_FILE}
+}
+
+_fatal() {
+    #set +Ex
+    #echo "$0: Error: $*" >&2
+    exit 1
+}
+
+# a replacement for systems, that don't have "which" command
+_which() {
+    local bin="$1"
+    echo $PATH| tr ':' '\n'| while read dir; do
+      if [ -x "$dir/$bin" ] ; then
+          echo "$dir/$bin"
+          return 0
+      fi
+    done
+    echo "$bin not found"
+    return 1                                                                                                                                                                   
+}  
+
+# validate ES is reachable
+_validate_environment() {
+    log "validate if ES is reachable"
+}
+
+_enable_dynamic_mapping() {
+    curl -XPUT -f ${ES_ENDPOINT}/_template/enable_dynamic_mappings -d '
+    {
+      "order": 0,
+      "template": "*",
+      "mappings": {
+         "_default_": {
+             "numeric_detection": true
+         }
+      } 
+    }'
+    if [ 0 -eq $? ]; then
+       log "enable dynamic mapping successfully"
+    else
+       log "failed to enable dynamic mapping"
+    fi
+}
+
+# create template for general navigation
+_create_navigation_template() {
+    log "create template for navigation"
+    curl -XPUT -f ${ES_ENDPOINT}/_template/navigation -d@./navigation_template.json
+    if [ 0 -eq $? ]; then
+       log "create template for navigation successfully"
+    else
+       log "failed to create template for navigation"
+    fi
+}
+
+# create navigation category
+_create_navigation_category() {
+    log "create navigation category"
+    curl -XPUT -f ${ES_ENDPOINT}/_bulk  --data-binary @./navigation_category.json
+    if [ 0 -eq $? ]; then
+       log "created navigation category successfully"
+    else
+       log "failed to create navigation category"
+    fi
+}
+
+# create template for accesslog 
+_create_accesslog_template() {
+    log "create template for accesslog"
+    curl -XPUT -f ${ES_ENDPOINT}/_template/accesslog -d@./accesslog_template.json
+    if [ 0 -eq $? ]; then
+       log "create template for accesslog successfully"
+    else
+       log "failed to create template for accesslog"
+    fi
+}
+
+# create template for metricbeat
+_create_metricbeat_template() {
+    log "create template for metricbeat"
+    curl -XPUT -f ${ES_ENDPOINT}/_template/metricbeat -d@./metricbeat_template.json
+    if [ 0 -eq $? ]; then
+       log "create template for metricbeat successfully"
+    else
+       log "failed to create template for metricbeat"
+    fi
+}
+
+# create template for ecsbeat
+_create_ecsbeat_template() {
+    log "create template for ecsbeat"
+    curl -XPUT -f ${ES_ENDPOINT}/_template/ecsbeat -d@./ecsbeat_template.json
+    if [ 0 -eq $? ]; then
+       log "create template for ecsbeat successfully"
+    else
+       log "failed to create template for ecsbeat"
+    fi
+}
+
+_create_accesslog_kibana_index() {
+    log "create index for accesslog"
+    curl -XPUT -f ${ES_ENDPOINT}/.kibana/index-pattern/filebeat-* -d@./filebeat_field_update.data
+    if [ 0 -eq $? ]; then
+       log "create kibana index for filebeat successfully"
+    else
+       log "failed to create kibana index for filebeat"
+    fi
+}
+
+_create_metricbeat_kibana_index() {
+    log "create index for metricbeat"
+    curl -XPUT -f ${ES_ENDPOINT}/.kibana/index-pattern/metricbeat-* -d@./metricbeat_field_update.data
+    if [ 0 -eq $? ]; then
+       log "create kibana index for metricbeat successfully"
+    else
+       log "failed to create kibana index for metricbeat"
+    fi
+}
+
+_create_ecsbeat_kibana_index() {
+    log "create index for ecsbeat"
+    curl -XPUT -f ${ES_ENDPOINT}/.kibana/index-pattern/ecsbeat-* -d@./ecsbeat_field_update.data
+    if [ 0 -eq $? ]; then
+       log "create kibana index for ecsbeat successfully"
+    else
+       log "failed to create kibana index for ecsbeat"
+    fi
+}
+
+_create_navigation_kibana_index() {
+    log "create index for navigation"
+    curl -XPUT -f ${ES_ENDPOINT}/.kibana/index-pattern/navigation -d@./navigation_field_update.data
+    if [ 0 -eq $? ]; then
+       log "create kibana index for navigation successfully"
+    else
+       log "failed to create kibana index for navigation"
+    fi
+}
+
+_import_kibana_visualization() {
+    log "import kibana visualization"
+    cd ${VISUALIZATION_PATH}
+    for i in `ls -1`; do 
+        local fname=`basename $i .json`
+        curl -XPUT -f ${ES_ENDPOINT}/.kibana/visualization/${fname} -d@$i
+        if [ 0 -ne $? ]; then
+          log "failed to import kibana visualization"
+          return
+        fi
+    done
+    log "import kibana visualization successfully"
+}
+
+_import_kibana_search() {
+    log "import kibana search"
+    cd ${SEARCH_PATH}
+    for i in `ls -1`; do 
+        local fname=`basename $i .json`
+        curl -XPUT -f ${ES_ENDPOINT}/.kibana/search/${fname} -d@$i
+        if [ 0 -ne $? ]; then
+          log "failed to import kibana search"
+          return
+        fi
+    done
+    log "import kibana search successfully"
+}
+
+_import_kibana_dashboard() {
+    log "import kibana dashboard"
+    cd ${DASHBOARD_PATH}
+    for i in `ls -1`; do 
+        local fname=`basename $i .json`
+        curl -XPUT -f ${ES_ENDPOINT}/.kibana/dashboard/${fname} -d@$i
+        if [ 0 -ne $? ]; then
+          log "failed to import kibana dashboard"
+          return
+        fi
+    done
+    log "import kibana dashboard successfully"
+}
+
+_create_ecsbeat_field_format_map() {
+   curl -XPOST -f ${ES_ENDPOINT}/.kibana/index-pattern/ecsbeat-*/_update -d '
+   {
+     "doc": {
+        "fieldFormatMap" : "{\"diskSpaceOfflineTotalCurrent_Space\":{\"id\":\"bytes\",\"params\":{\"pattern\":\"0,0b\"}},\"diskSpaceAllocatedCurrent_Space\":{\"id\":\"bytes\",\"params\":{\"pattern\":\"0,0b\"}},\"diskSpaceFreeCurrent_Space\":{\"id\":\"bytes\",\"params\":{\"pattern\":\"0,0b\"}},\"diskSpaceTotalCurrent_Space\":{\"id\":\"bytes\",\"params\":{\"pattern\":\"0,0b\"}}}"
+     }
+   }'
+   if [ 0 -eq $? ]; then
+     log "create ecsbeat field format map successfully"
+   else
+     log "failed to create ecsbeat field format map"
+   fi 
+}
