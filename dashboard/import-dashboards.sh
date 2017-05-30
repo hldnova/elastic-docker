51f08f5bdd145a4154608c1e83bbe7dac03c9fdb
Lida He <lida.he@emc.com>
2017-05-30 17:18:50 -0400

    update index

--- dashboard/import-dashboards.sh
+++ dashboard/import-dashboards.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ES_ENDPOINT=${ES_ENDPOINT:-'http://localhost:9200'}
 set -x
-bin/import_dashboards -es ${ES_ENDPOINT} -dir . -only-dashboards
+bin/import_dashboards -es http://localhost:9200 -dir . -only-dashboards
 
